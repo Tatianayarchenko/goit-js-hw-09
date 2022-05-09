090ce599d1ca0b7f042d8c3dae591b864cbd7fc5
Tatiana Yarchenko <ktanya61@gmail.com>
2022-05-09 12:40:41 +0300

    сделала первое задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+const getRandomHexColor = function () {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+const changeBackgroundColor = {
+  intervalId: null,
+
+  onStart() {
+    this.intervalId = setInterval(() => {
+      document.body.style.background = getRandomHexColor();
+    }, 1000);
+    // console.log('нажали на кнопку старт');
+    if (this.onStart) {
+      refs.startBtn.disabled = true;
+      refs.stopBtn.disabled = false;
+    }
+  },
+
+  onStop() {
+    clearInterval(this.intervalId);
+    if (this.onStop) {
+      refs.stopBtn.disabled = true;
+      refs.startBtn.disabled = false;
+    }
+    // console.log('нажали на кнопку стоп');
+  },
+};
+
+refs.startBtn.addEventListener('click', () => {
+  changeBackgroundColor.onStart();
+});
+refs.stopBtn.addEventListener('click', () => {
+  changeBackgroundColor.onStop();
+});
